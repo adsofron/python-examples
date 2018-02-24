19dd089006767562b43407ed4ee74fc8e9da8f50
Alina Sofron <adsofron@gmail.com>
2018-02-24 22:36:17 +0200

    Update .gitignore & longest-string.py

--- longest_string.py
+++ longest_string.py
@@ -6,17 +6,15 @@ Created on Sun Dec  3 16:50:19 2017
 """
 
 
-s = "azcbobobegghakl"
+s = "abdefhabckfor"
 n = 0
 x = 0
 word =""
 for i in range(1,len(s)-1):
-    if s[i-1] < s[i] :
-        word= word +s[i-1] + s[i]
+    if s[i] > s[i-1] :
+        word= word + s[i]
         print(word)
-       
-    elif s[i-1] > s[i] or s[i] == s[i]:
-        n = n+1 
-         
+    else:
+        break
 print("Longest substring in alphabetical order is: " ,word)
-print("n = ",n)
+
